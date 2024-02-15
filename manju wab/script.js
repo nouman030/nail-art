9352666f6ab5d728bee42a131df8b876b7e05893
nouman030 <159903164+nouman030@users.noreply.github.com>
2024-02-15 18:25:08 +0530

    Add files via upload

---
+++ manju wab/script.js
@@ -0,0 +1,9 @@
+// JavaScript for Navigation Bar
+function show() {
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hide() {
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
